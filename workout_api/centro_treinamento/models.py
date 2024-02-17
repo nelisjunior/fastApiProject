4b0ac7ca94d99e92c38e5f183dd6543ee54105e4
Nelis Junior <dev@nelis.me>
2024-02-17 16:39:58 -0300

    Aplicação funcional

--- workout_api/centro_treinamento/models.py
+++ workout_api/centro_treinamento/models.py
@@ -1,18 +1,14 @@
-from typing import Annotated
-
-from pydantic import Field, UUID4
-from workout_api.contrib.schemas import BaseSchema
-
-
-class CentroTreinamentoIn(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
-    endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua X, Q02', max_length=60)]
-    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', example='Marcos', max_length=30)]
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workout_api.contrib.models import BaseModel
+from workout_api.atleta.models import AtletaModel
 
 
-class CentroTreinamentoAtleta(BaseSchema):
-    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+class CentroTreinamentoModel(BaseModel):
+    __tablename__ = 'centros_treinamento'
 
-
-class CentroTreinamentoOut(CentroTreinamentoIn):
-    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    endereco: Mapped[str] = mapped_column(String(60), nullable=False)
+    proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
+    atleta: Mapped['AtletaModel'] = relationship(back_populates='centro_treinamento')
--- workout_api/contrib/dependencies.py
+++ workout_api/contrib/dependencies.py
@@ -1,8 +1,8 @@
-from uuid import uuid4
-from sqlalchemy import UUID
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.dialects.postgresql import UUID as PG_UUID
+from typing import Annotated
+from fastapi import Depends
 
+from sqlalchemy.ext.asyncio import AsyncSession
 
-class BaseModel(DeclarativeBase):
-    id: Mapped[UUID] = mapped_column(PG_UUID(as_uuid=True), default=uuid4, nullable=False)
+from workout_api.configs.database import get_session
+
+DatabaseDependency = Annotated[AsyncSession, Depends(get_session)]
